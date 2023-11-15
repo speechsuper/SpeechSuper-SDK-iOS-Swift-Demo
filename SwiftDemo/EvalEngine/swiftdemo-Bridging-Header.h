d5546e4aae35b39d5b4769bc72d2e2759bb89a4e
Qiusi Dong <qiusidong@Qiusis-MacBook-Pro.local>
2023-11-15 13:46:20 +0800

    add files

---
+++ SwiftDemo/EvalEngine/swiftdemo-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  swiftdemo-Bridging-Header.h
+//  RecorderSwiftDemo
+//
+//  Created by Mac on 2023/8/15.
+//
+
+#ifndef swiftdemo_Bridging_Header_h
+#define swiftdemo_Bridging_Header_h
+#import "skegn.h"
+#endif /* swiftdemo_Bridging_Header_h */
